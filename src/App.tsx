fc3593b323002f0bbce8c2c7adf354e0caef6955
Max <zeetreex@gmail.com>
2021-12-02 21:17:03 +0200

    adding todo function, controlled input

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import {useState} from 'react'
 import { Todo } from "./types/data"
 const App: React.FC = () => {
+
+    const [value, setValue] = useState("");
+    const [todos, setTodos] = useState<Todo[]>([])
+
+    const addTodo = () => {
+        setTodos([...todos, {
+            id: Date.now(),
+            title: value,
+            complete: false
+        }])
+        setValue('')
+    }
+
     return (
         <div>
-
+            <div>
+                <input type="text" onChange={(e => setValue(e.target.value))} value={value}/>
+                <button onClick={addTodo}> Add </button>
+            </div>
+            {todos.map((todo) => {
+                return <div>{todo.title}</div>
+            })}
         </div>
     );
 };
